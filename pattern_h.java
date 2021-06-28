bc1d4228f23840e5c86fbf8a8471e6d4c7a9b002
Raunit Singh <83498401+RaunitS@users.noreply.github.com>
2021-06-28 23:07:34 +0530

    Add files via upload

---
+++ pattern_h.java
@@ -0,0 +1,30 @@
+package forloops;
+
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+
+		int n= 10;
+		for(int i=1;i<=5; i++) {
+			System.out.println("h");
+			
+		}
+		for(int i=1; i<=5;i++) {
+		System.out.print("h");
+		}
+		System.out.println();
+		for (int i=1;i<=5;i++) {
+			System.out.print("h");
+			for(int j=1;j<=3;j++) {
+				System.out.print(" ");
+			}
+		System.out.println("h");
+			
+		}
+		
+		
+
+	}
+
+}
