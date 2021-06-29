718674e16359db89a9d4b7a5b7001bb15d4353d2
Raunit Singh <83498401+RaunitS@users.noreply.github.com>
2021-06-29 22:37:13 +0530

    Add files via upload

---
+++ Evennumbers.java
@@ -0,0 +1,25 @@
+package forloops;
+import java.util.Scanner;
+
+public class Evennumbers {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		System.out.println("enter any number");
+		int n = input.nextInt();
+		if(n%2==0) {
+			for(int i=0;i<= n;i=i+2) {
+				System.out.print(i+" ");
+			}
+		}
+			else {
+				for(int i=0;i<= n-1;i=i+2) {
+					System.out.print(i+" ");
+				}
+				
+			}
+		}
+		
+}
+
+
