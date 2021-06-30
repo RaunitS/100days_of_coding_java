68dff361463153985d464e84ea5a9be2ff4f7003
Raunit Singh <83498401+RaunitS@users.noreply.github.com>
2021-06-30 22:24:50 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,19 @@
+package forloops;
+import java.util.Scanner;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter the number you want to find the factorial");
+		int n= input.nextInt();
+		int fact=1;
+		for(int i=1;i<=n;i++) {
+			fact=fact*i;
+		}
+		System.out.println("factorial of n is "+ fact);
+		
+		
+	}
+
+}
